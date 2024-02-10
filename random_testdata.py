a16e94426273acb5460c0c1b9e9a7fb79d4edf50
Andreas Reich <r_andreas2@web.de>
2024-02-10 11:04:54 +0100

    updated testdata thing

--- random_testdata.py
+++ random_testdata.py
@@ -1,13 +1,18 @@
 def write_memh(data, filename):
-    with open("weights.memh", "w") as f:
-        for m in memory:
+    with open(filename, "w") as f:
+        for m in data:
             f.write(f"{m:0{4}x} ")
 
 
-size_in_bytes = 256
+size_in_bytes = 256 * 2
 
-memory = [i for i in range(size_in_bytes)]
-print("sum: ", sum(memory))
+weights = [i for i in range(size_in_bytes)]
+activations = [i for i in range(size_in_bytes)]
 
-write_memh(memory, "weights.memh")
-write_memh(memory, "activations.memh")
+accumulator = 0
+for w, a in zip(weights, activations):
+    accumulator += w * a
+print(accumulator)
+
+write_memh(weights, "weights.memh")
+write_memh(activations, "activations.memh")
