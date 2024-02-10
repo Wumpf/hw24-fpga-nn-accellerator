fb042c7a3c38328326e2d97a098ff8097941b5b3
Joel Pryde <joelpryde@gmail.com>
2024-02-10 19:04:19 +0100

    Do training with giphy sequence

--- training/train_giphy.py
+++ training/train_giphy.py
@@ -14,69 +14,70 @@ print(f"Devices: {devices}")
 rr.init("train_image", spawn=True)
 
 # Open the animated GIF
-frames_resized = []
+frames = []
 with Image.open('giphy.gif') as img:
     keep_frame_if_zero = 0
     for frame in ImageSequence.Iterator(img):
-        keep_frame_if_zero = (keep_frame_if_zero + 1) % 3
+        keep_frame_if_zero = (keep_frame_if_zero + 1) % 5
         if keep_frame_if_zero != 0:
             continue
-        frame_resized = frame.resize((64, 64))
-        frame_resized = frame_resized.convert('RGB')
-        frames_resized.append(frame_resized)
+        frame = frame.resize((64, 64))
+        frame = frame.convert('RGB')
+        frames.append(frame)
 
+# Load the image to calculate the width and height
+image = frames[0]
+width, height = frames[0].size
 
-# Load the image
-image = frames_resized[0]
-width, height = image.size
+rr.set_time_seconds("frame", 0)
+rr.log("train_input_all", rr.Image(np.reshape(frames, ((width) * len(frames), (height), 3)).astype(np.uint8)))
 
-# image = np.zeros((width, height, 3), dtype=np.uint8)
-# for y in range(width):
-#     image[:, y, 0] = np.linspace(0, 255, width)
-# for x in range(height):
-#     image[x, :, 1] = np.linspace(0, 255, height)
-
-
-for i in range(1, len(frames_resized)):
-    rr.set_time_sequence("frame", i)
-    rr.log("train_input", rr.Image(frames_resized[i]))
+# Log the input data
+for i in range(len(frames)):
+    rr.set_time_seconds("frame", i)
+    rr.log("train_input", rr.Image(frames[i]))
 
 # Prepare input data (X): Normalized pixel coordinates
 x_coords = np.arange(width).reshape(-1, 1) / width  # Normalize x coordinates
 y_coords = np.arange(height).reshape(-1, 1) / height  # Normalize y coordinates
+frame_coords = np.linspace(0, 1, num=len(frames)).reshape(-1, 1) / len(frames)  # Normalize frame time
 
-# rnd = np.random.rand(128)
-#X = np.array([np.array([[x,y] for i in range(128)]).flatten()
-#               for y in y_coords for x in x_coords])
-
-joel = 64
-def gen_input(x_coords, y_coords):
-
+input_range = 64
+def gen_input(x_coords, y_coords, frame_coords):
     # the sin functions introduce artifacts at the bottom
-    return np.array([np.array([[np.sin(x * i), np.sin(y * i)] for i in range(joel)]).flatten()
-                for y in y_coords for x in x_coords])
+    return np.array([
+        np.array([
+            [
+                np.sin(x * i),
+                np.sin(y * i),
+                np.cos(f * i)
+            ] for i in range(input_range)
+            ]).flatten()
+        for y in y_coords for x in x_coords for f in frame_coords
+        ])
 
-X = gen_input(x_coords, y_coords)
+X = gen_input(x_coords, y_coords, frame_coords)
 rr.log("log", rr.TextLog(str(X.shape)))
 #rr.log("input_vec", rr.Tensor(np.reshape(X, (256, 128, 128))))
 
 # Prepare output data (Y): Normalized RGB values
-pixels = np.array(image) / 255.0  # Normalize pixel values
+pixels = np.array(frames) / 255.0  # Normalize pixel values
 Y = pixels.reshape(-1, 3)  # Reshape to a 2D array where each row is RGB
+
 
 print(f"X.shape : {X.shape}")
 print(f"Y.shape : {Y.shape}")
 
 layer_size = 64
 model = Sequential([
-    Dense(layer_size, input_dim=joel * 2, activation='relu'),
+    Dense(layer_size, input_dim=input_range * 3, activation='relu'),
     Dense(layer_size, activation='relu'),
     #Dense(layer_size, activation='relu'),
     Dense(3, activation='sigmoid')  # Use 'sigmoid' to output values between 0 and 1
 ])
 
 model.compile(optimizer='nadam', loss='mean_squared_error')
-history = model.fit(X, Y, epochs=100, batch_size=32, verbose=True, validation_split=.1)  # Adjust epochs and batch size as needed
+history = model.fit(X, Y, epochs=20, batch_size=32, verbose=True, validation_split=.1)  # Adjust epochs and batch size as needed
 
 # calculate distribution of weights of layers
 w_dict = {}
@@ -113,27 +114,24 @@ def convert_to_fxp():
 
 convert_to_fxp()
 
+num_output_frames = len(frames) * 2
 #x_coords = np.linspace(-64, 128, num=64+128).reshape(-1, 1) / width  # Normalize x coordinates
 #y_coords = np.linspace(-64, 128, num=64+128).reshape(-1, 1) / height  # Normalize y coordinates
-X = gen_input(x_coords, y_coords)
+frame_coords = np.linspace(0, 1, num=num_output_frames).reshape(-1, 1) / len(frames)  # Normalize frame time
+
+X = gen_input(x_coords, y_coords, frame_coords)
 
 predicted_rgb = model.predict(X) * 255  # Rescale the output
-predicted_rgb = predicted_rgb.reshape((len(x_coords), len(y_coords), 3)).astype(np.uint8)
+
+rr.log("log", rr.TextLog(str(predicted_rgb.shape)))
+
+predicted_rgb = predicted_rgb.reshape((len(x_coords) * num_output_frames, len(y_coords), 3)).astype(np.uint8)
 
 # Create and save the generated image
-generated_image = Image.fromarray(predicted_rgb)
-# generated_image.save('training_output.png')
-# generated_image.show()
+rr.set_time_seconds("frame", 0)
+rr.log("train_output_all", rr.Image(predicted_rgb))
 
-# Plot the training and validation loss
-# plt.figure(figsize=(16, 8))
-# plt.plot(history.history['loss'], label='train loss')
-# plt.plot(history.history['val_loss'], label='validation loss')
-# plt.title('Model Loss')
-# plt.ylabel('Loss')
-# plt.xlabel('Epoch')
-# plt.legend()
-# plt.show()
-
-
-rr.log("train_output", rr.Image(generated_image))
+for i in range(num_output_frames):
+    time = i / num_output_frames * len(frames)
+    rr.set_time_seconds("frame", time)
+    rr.log("train_output", rr.Image(predicted_rgb[i*len(x_coords):(i+1)*len(x_coords), :, :]))
