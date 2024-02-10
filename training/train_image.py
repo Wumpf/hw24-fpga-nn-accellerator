40e903840c35dbf92e6b1bf8987bac1ddc814712
Joel Pryde <joelpryde@gmail.com>
2024-02-10 16:02:10 +0100

    WIP on stuff

--- training/train_image.py
+++ training/train_image.py
@@ -1,7 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageSequence
 import matplotlib.pyplot as plt
 import rerun as rr
 from fxpmath import Fxp
@@ -13,20 +13,23 @@ print(f"Devices: {devices}")
 
 rr.init("train_image", spawn=True)
 
-# Load an animated gif and convert it to a sequence of images
-# image_path = 'face2_input.gif'
+# Open the animated GIF
+frames_resized = []
+with Image.open('giphy.gif') as img:
+    keep_frame_if_zero = 0
+    for frame in ImageSequence.Iterator(img):
+        keep_frame_if_zero = (keep_frame_if_zero + 1) % 3
+        if keep_frame_if_zero != 0:
+            continue
+        frame_resized = frame.resize((64, 64))
+        frame_resized = frame_resized.convert('RGB')
+        frames_resized.append(frame_resized)
+
 
 # Load the image
-image_path = 'face_training.png'
-image = Image.open(image_path)
-image = image.resize((64, 64))
+image = frames_resized[0]
 width, height = image.size
 
-# Convert the image to RGB (if it's not already in RGB)
-image = image.convert('RGB')
-
-# width = 128
-# height = 128
 # image = np.zeros((width, height, 3), dtype=np.uint8)
 # for y in range(width):
 #     image[:, y, 0] = np.linspace(0, 255, width)
@@ -34,7 +37,9 @@ image = image.convert('RGB')
 #     image[x, :, 1] = np.linspace(0, 255, height)
 
 
-rr.log("train_input", rr.Image(image))
+for i in range(1, len(frames_resized)):
+    rr.set_time_sequence("frame", i)
+    rr.log("train_input", rr.Image(frames_resized[i]))
 
 # Prepare input data (X): Normalized pixel coordinates
 x_coords = np.arange(width).reshape(-1, 1) / width  # Normalize x coordinates
@@ -44,12 +49,14 @@ y_coords = np.arange(height).reshape(-1, 1) / height  # Normalize y coordinates
 #X = np.array([np.array([[x,y] for i in range(128)]).flatten()
 #               for y in y_coords for x in x_coords])
 
+joel = 64
+def gen_input(x_coords, y_coords):
 
-joel = 128
-# the sin functions introduce artifacts at the bottom
-X = np.array([np.array([[np.sin(x * i), np.sin(y * i)] for i in range(joel)]).flatten()
-               for y in y_coords for x in x_coords])
+    # the sin functions introduce artifacts at the bottom
+    return np.array([np.array([[np.sin(x * i), np.sin(y * i)] for i in range(joel)]).flatten()
+                for y in y_coords for x in x_coords])
 
+X = gen_input(x_coords, y_coords)
 rr.log("log", rr.TextLog(str(X.shape)))
 #rr.log("input_vec", rr.Tensor(np.reshape(X, (256, 128, 128))))
 
@@ -62,7 +69,7 @@ print(f"Y.shape : {Y.shape}")
 
 layer_size = 64
 model = Sequential([
-    Dense(layer_size, input_dim=256, activation='relu'),
+    Dense(layer_size, input_dim=joel * 2, activation='relu'),
     Dense(layer_size, activation='relu'),
     #Dense(layer_size, activation='relu'),
     Dense(3, activation='sigmoid')  # Use 'sigmoid' to output values between 0 and 1
@@ -106,8 +113,12 @@ def convert_to_fxp():
 
 convert_to_fxp()
 
+#x_coords = np.linspace(-64, 128, num=64+128).reshape(-1, 1) / width  # Normalize x coordinates
+#y_coords = np.linspace(-64, 128, num=64+128).reshape(-1, 1) / height  # Normalize y coordinates
+X = gen_input(x_coords, y_coords)
+
 predicted_rgb = model.predict(X) * 255  # Rescale the output
-predicted_rgb = predicted_rgb.reshape((height, width, 3)).astype(np.uint8)
+predicted_rgb = predicted_rgb.reshape((len(x_coords), len(y_coords), 3)).astype(np.uint8)
 
 # Create and save the generated image
 generated_image = Image.fromarray(predicted_rgb)
