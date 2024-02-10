c3ced8a0f75d0ae363bc6a6d4762cd7938f3053d
Gabriele Farina <me@gabrielefarina.com>
2024-02-10 19:15:21 +0100

    Cleanup

---
+++ training/common.py
@@ -0,0 +1,123 @@
+import os
+import shutil
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+def gen_gradient_image(w, h):
+    """Generate a gradient image of size w x h."""
+    image = np.zeros((w, h, 3), dtype=np.uint8)
+    for y in range(w):
+        image[:, y, 0] = np.linspace(0, 255, w)
+    for x in range(h):
+        image[x, :, 1] = np.linspace(0, 255, h)
+    return image
+
+class NeuralNetwork(object):
+    def __init__(self, img_width, img_height, layer_size, embedding_size) -> None:
+        self.__img_width = img_width
+        self.__img_height = img_height
+        self.__layer_size = layer_size
+        self.__embedding_size = embedding_size
+
+        self.__x_coords = np.arange(self.__img_width).reshape(-1, 1) / self.__img_width
+        self.__y_coords = np.arange(self.__img_height).reshape(-1, 1) / self.__img_height
+
+        self.__encoded_pos = self.__encode_positions()
+
+        self.__model = Sequential([
+            Dense(layer_size, input_dim=embedding_size*2, activation='relu'),
+            Dense(layer_size, activation='relu'),
+            Dense(layer_size, activation='relu'),
+            Dense(3, activation='relu')
+        ])
+        self.__model.compile(optimizer='nadam', loss='mean_squared_error')
+    
+    def __encode_positions(self):
+        seed = 127
+        np.random.seed(seed)
+        rnd = np.random.random_integers(0, 0xFFFFFF, self.__embedding_size)
+        return np.array([
+            np.array([
+                [np.sin(x * rnd[i] + y), np.cos(y * rnd[self.__embedding_size-1-i] + x)]
+                    for i in range(self.__embedding_size)]).flatten()
+                        for y in self.__x_coords for x in self.__y_coords])
+
+    def train(self, image, epochs=100, batch_size=32):
+        Y = (np.array(image) / 255.0).reshape(-1, 3)
+        return self.__model.fit(self.__encoded_pos, Y, epochs=epochs, batch_size=batch_size, verbose=True, validation_split=.1)  # Adjust epochs and batch size as needed
+
+    def __load_ascii(self, folder, name):
+        with open(f"{folder}/{name}.txt", "r") as f:
+            floats = np.array([float(int(x, 16)) for x in f.read().strip().split(" ")])
+            floats -= 127
+            floats /= 128.0
+            return floats
+        
+    def __load_ascii_reshape(self, folder, name, x, y):
+        with open(f"{folder}/{name}.txt", "r") as f:
+            floats = np.array([float(int(x, 16)) for x in f.read().strip().split(" ")])
+            floats -= 127
+            floats /= 128.0
+            return floats.reshape((x, y))
+
+    def load_weights_from_folder(self, folder):
+        layers = {
+                "dense": (self.__load_ascii_reshape(folder, "dense_weights", 64, 64), self.__load_ascii(folder, "dense_biases")),
+                "dense_1": (self.__load_ascii_reshape(folder, "dense_1_weights", 64, 64), self.__load_ascii(folder, "dense_1_biases")),
+                "dense_2": (self.__load_ascii_reshape(folder, "dense_2_weights", 64, 64), self.__load_ascii(folder, "dense_2_biases")),
+                "dense_3": (self.__load_ascii_reshape(folder, "dense_3_weights", 64, 3), self.__load_ascii(folder, "dense_3_biases"))
+            }
+        for layer, values in layers.items():
+                self.__model.get_layer(layer).set_weights(values)
+
+    def predict(self):
+        predicted_rgb = self.__model.predict(self.__encoded_pos) * 255  # Rescale the output
+        return predicted_rgb.reshape((self.__img_height, self.__img_width, 3)).astype(np.uint8)
+
+    def __quantize_parameters(self):
+        w_dict = {}
+        for layer in self.__model.layers:
+            w_dict[layer.name] = self.__model.get_layer(layer.name).get_weights()
+
+        quantized_params = {}
+        for layer in w_dict.keys():
+            weights, biases = w_dict[layer]
+
+            d = 128.0
+
+            weights = np.clip(weights, -1.0, 1.0)
+            weights *= d
+            weights = np.floor(weights)
+            weights /= d
+
+            biases = np.clip(biases, -1.0, 1.0)
+            biases *= d
+            biases = np.floor(biases)
+            biases /= d
+
+            quantized_params[layer] = (weights, biases)
+        
+        return quantized_params
+
+    def __write_array_to_memh_file(self, values, name, folder):
+        with open(os.path.join(folder, f"{name}.txt"), "w") as f:
+            for m in values.flatten() * 128.0 + 127.0:
+                f.write(f"{int(m):0{2}x} ")
+
+    def __write_weights(self, quantized_params, folder):
+        """Write the weights to binary files."""
+        if os.path.exists(folder):
+            shutil.rmtree(folder)
+            
+        os.makedirs(folder, exist_ok=True)
+        for layer, values in quantized_params.items():
+            self.__write_array_to_memh_file(values[0], f"{layer}_weights", folder)
+            self.__write_array_to_memh_file(values[1], f"{layer}_biases", folder)
+    
+    def write_quantized_weights(self, folder):
+        quantized_params = self.__quantize_parameters()
+        for layer, values in quantized_params.items():
+            self.__model.get_layer(layer).set_weights(values)
+        self.__write_weights(quantized_params, folder)
--- training/gen_image.py
+++ training/gen_image.py
@@ -1,15 +1,10 @@
-import os
-import shutil
 import rerun as rr
 
 import numpy as np
-from fxpmath import Fxp
-import tensorflow as tf
 
 from PIL import Image
 
-from keras.models import Sequential
-from keras.layers import Dense
+from common import NeuralNetwork
 
 def load_ascii(name):
     with open(f"output/{name}.txt", "r") as f:
@@ -38,48 +33,15 @@ def set_weights(model, layers):
     for layer, values in layers.items():
         model.get_layer(layer).set_weights(values)
 
-# List all available devices
-devices = tf.config.list_physical_devices()
-print(f"Devices: {devices}")
-
 rr.init("test_ascii", spawn=True)
 
 width, height = 64, 64
-
-# Prepare input data (X): Normalized pixel coordinates
-x_coords = np.arange(width).reshape(-1, 1) / width
-y_coords = np.arange(height).reshape(-1, 1) / height
-
-def x_training_data_rnd_sin_cos_biased(x_coords, y_coords, embedding_size):
-    seed = 127
-    np.random.seed(seed)
-    rnd = np.random.random_integers(0, 0xFFFFFF, embedding_size)
-    return np.array([np.array([[np.sin(x * rnd[i] + y), np.cos(y * rnd[embedding_size-1-i] + x)] for i in range(embedding_size)]).flatten() for y in y_coords for x in x_coords])
-
 embedding_size = 32
 layer_size = 64
 
-X = x_training_data_rnd_sin_cos_biased(x_coords, y_coords, embedding_size)
-
-rr.log("log", rr.TextLog(str(X.shape)))
-
-print(f"X.shape : {X.shape}")
-
-model = Sequential([
-    Dense(layer_size, input_dim=embedding_size*2, activation='relu'),
-    Dense(layer_size, activation='relu'),
-    Dense(layer_size, activation='relu'),
-    Dense(3, activation='relu')
-])
-
-model.compile(optimizer='nadam', loss='mean_squared_error')
-
-set_weights(model, load_weights_and_bises())
-
-predicted_rgb = model.predict(X) * 255  # Rescale the output
-predicted_rgb = predicted_rgb.reshape((height, width, 3)).astype(np.uint8)
-
-# Create and save the generated image
+nn = NeuralNetwork(width, height, layer_size, embedding_size)
+nn.load_weights_from_folder("output")
+predicted_rgb = nn.predict()
 generated_image = Image.fromarray(predicted_rgb)
 
 rr.log("train_output", rr.Image(generated_image))
