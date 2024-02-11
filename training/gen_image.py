cde43b888e546c33a05dc8883f9627f60b179c77
Gabriele Farina <me@gabrielefarina.com>
2024-02-11 08:41:34 +0100

    Store activations when generating images

--- training/gen_image.py
+++ training/gen_image.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from PIL import Image
 
-from common import NeuralNetwork
+from common import NeuralNetwork, write_array_to_memh_file, quantize_array
 
 rr.init("test_ascii", spawn=True)
 
@@ -21,8 +21,13 @@ layer_size = 64
 
 nn = NeuralNetwork(width, height, layer_size, embedding_size, channels)
 
-nn.load_weights_from_folder("output")
+nn.load_weights_from_folder("output/with_eyes")
 predicted_rgb = nn.predict()
 generated_image = Image.fromarray(predicted_rgb, 'RGBA')
 
 rr.log("train_output", rr.Image(generated_image))
+
+activations = nn.predict_with_activations()
+
+for i, a in enumerate(activations):
+    write_array_to_memh_file(quantize_array(a), f"activation_{i}", "output/with_eyes")
