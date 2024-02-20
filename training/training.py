16d367ed8ece0049cf8c17122bb4f74f6d827787
rej <rzioma@acm.org>
2024-02-20 22:51:39 +0100

    "quantized_output" in rerun log, optional mixed precision in training

--- training/training.py
+++ training/training.py
@@ -14,7 +14,8 @@ else:
     channels = 3
 
 # Load the image
-image_path = 'face_input.png'
+image_path = 'pretty_256.png'
+#image_path = 'face_input.png'
 #image_path = 'training_input.png'
 image = Image.open(image_path)
 image = image.resize((64, 64))
@@ -25,14 +26,20 @@ rr.log("train_input", rr.Image(image))
 
 embedding_size = 32
 layer_size = 64
+
+# nn = NeuralNetwork(width, height, layer_size, embedding_size, channels, mixed_precision="mixed_float16")
 nn = NeuralNetwork(width, height, layer_size, embedding_size, channels)
 
 history = nn.train(image, 200, 32)
 
-nn.write_quantized_weights("output")
-
 predicted_rgba = nn.predict()
-
 generated_image = Image.fromarray(predicted_rgba, mode)
 
 rr.log("train_output", rr.Image(generated_image))
+
+nn.write_quantized_weights("output")
+
+q_predicted_rgba = nn.predict()
+q_generated_image = Image.fromarray(q_predicted_rgba, mode)
+
+rr.log("quantized_output", rr.Image(q_generated_image))
